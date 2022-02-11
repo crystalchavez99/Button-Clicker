bf4c4987ba6fc77ff619bd6dcd692768f6338d11
Crystal Chavez <88100670+crystalchavez99@users.noreply.github.com>
2022-02-10 18:36:11 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
